26b276f200df835277b5556cb4c8f68e7575d1f5
moajb990 <86415810+moajb990@users.noreply.github.com>
2021-06-25 00:18:13 +0300

    Update moajb.py

--- moajb.py
+++ moajb.py
@@ -11,32 +11,32 @@ oo=set([])
 pop=set([])
 cc=1
 while True :
-     b=input('Entre {} : '.format(kk))
-     if b=='exit' :
-         print('\033[3;36m')
-         file.close()
-         qq=open(mm, 'r' )
-         jj=len(qq.readlines())
-         os.system('printf "\033[3;31m"')
-         print('-'*60)
-         print('>> {} Passwords in ---> {} '.format(ll, mm))
-         print('-'*60)
-         break ;
-     oo.add(b)
-     for i in oo:
-         if len(i) >= 6 and i not in pop :
-             pop.add(i)
-             file.write(i)
-             file.write('\n')
-         c=b+i
-         q=i+b
-         if len(c) >= 6 :
-             file.write(c)
-             file.write('\n')
+    b=input('Entre {} : '.format(kk))
+    if b=='exit' :
+        print('\033[3;36m')
+        file.close()
+        qq=open(mm, 'r' )
+        jj=len(qq.readlines())
+        os.system('printf "\033[3;31m"')
+        print('-'*60)
+        print('>> {} Passwords in ---> {} '.format(ll, mm))
+        print('-'*60)
+        break ;
+    oo.add(b)
+    for i in oo:
+        if len(i) >= 6 and i not in pop :
+            pop.add(i)
+            file.write(i)
+            file.write('\n')
+        c=b+i
+        q=i+b
+        if len(c) >= 6 :
+            file.write(c)
+            file.write('\n')
          if c != d and len(d) >= 6:
-         file.write(d)
-         file.write('\n')
+           file.write(d)
+           file.write('\n')
 
        
-     cc=int(cc)+1
-     print('-'*40)
+    cc=int(cc)+1
+    print('-'*40)
